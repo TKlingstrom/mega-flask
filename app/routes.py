86b923feb5da85a36dec177eb0f8efcf9ded8003
TKlingstrom <tomas.klingstrom@slu.se>
2018-01-16 19:53:24 +0100

    Solved bugs in routes and templates file

--- app/routes.py
+++ app/routes.py
@@ -6,14 +6,13 @@ from app import app
 def index():
     user = {'username': 'Miguel'}
     posts = [
-      {
-        'author': {'username': 'John'},
-        'body': 'Beautiful day in Portland!'
-      },
-      {
-        'author' {'username': 'Susan'},
-        'body': 'The Avengers movie was so cool!'
-
-      }
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
